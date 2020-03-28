8972e3d4f02d6ca233a591aabde5b94318adfc04
qasimgulzar <qasim.khokhar52@gmail.com>
2020-03-28 09:54:50 +0500

    [FIX] localstack s3 images are not working.

--- e_store_primzel/settings/aws.py
+++ e_store_primzel/settings/aws.py
@@ -1,11 +1,15 @@
-AWS_ACCESS_KEY_ID = '123'
-AWS_SECRET_ACCESS_KEY = 'xyz'
-AWS_STORAGE_BUCKET_NAME = 'demo-bucket'
-AWS_S3_ENDPOINT_URL = 'http://localhost:4572'
-AWS_S3_CUSTOM_DOMAIN = '%s.localhost:4572' % AWS_STORAGE_BUCKET_NAME
+import os
+
+THUMBNAIL_DEBUG = True
+THUMBNAIL_PRESERVE_FORMAT = True
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '123')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID', 'xyz')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', 'demo-bucket')
+AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL', 'http://localhost:4572/')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
-
 DEFAULT_FILE_STORAGE = 'backends.storage_backends.MediaStorage'
-MEDIA_URL='/demo-bucket/media/'
+AWS_S3_SECURE_URLS = os.getenv('AWS_S3_SECURE_URLS', False)
+AWS_PRELOAD_METADATA = os.getenv('AWS_PRELOAD_METADATA', False)
