83413c64a58bbdd52125c4fa65be0efc81c72780
Qasim Khokhar <qasim.khokhar52@gmail.com>
2020-05-25 15:00:15 +0500

    [NEW] setup demo database.

--- .env.sh
+++ .env.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 source .env/bin/activate
 
 export DJANGO_SETTINGS_MODULE=e_store_primzel.settings
-export PRIMZEL_DEBUG=True
-export POSTGRES_HOST=localhost
-export POSTGRES_USER=qasim
-export POSTGRES_PASSWORD=qasim
+export PRIMZEL_DEBUG=False
+export POSTGRES_HOST=primzel-demo-rds.c2irzwqclpkg.us-east-2.rds.amazonaws.com
+export POSTGRES_USER=primzel
+export POSTGRES_PASSWORD=Primzel44332211
