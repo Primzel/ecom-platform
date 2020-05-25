2dfe5a24f98a0ac06e62bd63868e6af8e638756d
Qasim Khokhar <qasim.khokhar52@gmail.com>
2020-05-25 15:35:23 +0500

    [SETUP] asw s3 keys

--- .env.sh
+++ .env.sh
@@ -6,3 +6,5 @@ export PRIMZEL_DEBUG=False
 export POSTGRES_HOST=primzel-demo-rds.c2irzwqclpkg.us-east-2.rds.amazonaws.com
 export POSTGRES_USER=primzel
 export POSTGRES_PASSWORD=Primzel44332211
+export AWS_ACCESS_KEY_ID=AKIAJLCUH4TDT3G4ORYQ
+export AWS_SECRET_ACCESS_KEY=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
