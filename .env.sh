0afece5ca48e14e62e3fef65d1a15534eb254cbc
Qasim Khokhar <qasim.khokhar52@gmail.com>
2020-05-25 15:48:28 +0500

    [SETUP] asw s3 host and bucket

--- .env.sh
+++ .env.sh
@@ -8,3 +8,5 @@ export POSTGRES_USER=primzel
 export POSTGRES_PASSWORD=Primzel44332211
 export AWS_ACCESS_KEY_ID=AKIAJLCUH4TDT3G4ORYQ
 export AWS_SECRET_ACCESS_KEY=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
+export AWS_STORAGE_BUCKET_NAME=primzel-demo
+export AWS_S3_ENDPOINT_URL=https://primzel-demo.s3.us-east-2.amazonaws.com/
