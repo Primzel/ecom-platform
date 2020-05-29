1722f4600458e3166d95bec1cc2dcadccd32cfb3
qasimgulzar <qasim.khokhar52@gmail.com>
2020-05-30 01:37:35 +0500

    [FIX] env

--- .env.sh
+++ .env.sh
@@ -2,11 +2,11 @@
 source .env/bin/activate
 
 export DJANGO_SETTINGS_MODULE=e_store_primzel.settings
-export PRIMZEL_DEBUG=True
-export POSTGRES_HOST=localhost
-export POSTGRES_USER=primzel_store
-export POSTGRES_PASSWORD=primzel_store
-#export AWS_ACCESS_KEY_ID=123
-#export AWS_SECRET_ACCESS_KEY=123
-#export AWS_STORAGE_BUCKET_NAME=primzel-demo
-#export AWS_S3_ENDPOINT_URL=https://primzel-demo.s3.us-east-2.amazonaws.com/
+export PRIMZEL_DEBUG=False
+export POSTGRES_HOST=primzel-demo-rds.c2irzwqclpkg.us-east-2.rds.amazonaws.com
+export POSTGRES_USER=primzel
+export POSTGRES_PASSWORD=Primzel44332211
+export AWS_ACCESS_KEY_ID=AKIAJLCUH4TDT3G4ORYQ
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_STORAGE_BUCKET_NAME=primzel-demo
+export AWS_S3_ENDPOINT_URL=https://primzel-demo.s3.us-east-2.amazonaws.com/
