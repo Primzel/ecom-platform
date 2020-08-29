cc96ebd2431fad84f72070bf3f0c71cbb835f128
qasimgulzar <qasim.khokhar52@gmail.com>
2020-08-29 13:31:01 +0500

    BUG PRIM-9 0h 10m Catalog : Shop by brand section should be dynamic.

--- apps/partner/templatetags/partner_tags.py
+++ apps/partner/templatetags/partner_tags.py
@@ -6,6 +6,8 @@ Partner = get_model("partner", "Partner")
 
 
 @register.simple_tag(name="partners")  # noqa: C901 too complex
-def get_partner_list():
+def get_partner_list(offset=0, limit=10):
     partners = Partner.objects.all()
+    if offset >= 0 and limit >= 0:
+        partners=partners[offset:limit]
     return partners
