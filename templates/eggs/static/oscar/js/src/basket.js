279d9791dafe211f08b3b63a64aefd897b4c8dfd
qasimgulzar <qasim.khokhar52@gmail.com>
2021-03-06 20:48:06 +0500

    [MISSING] PRIM-46 Product card not listing variants as expected.

---
+++ templates/eggs/static/oscar/js/src/basket.js
@@ -0,0 +1,15 @@
+function addToBasketFormHandler(form) {
+    var selected = [];
+    $($(form).serializeArray()).each(function (index, field) {
+        if (['quantity', 'csrfmiddlewaretoken'].indexOf(field['name']) == -1) {
+            selected.push(field['value'].split(','));
+        }
+    });
+    var selected_child = _.intersection( selected);
+    if (selected_child) {
+        selected_child = selected_child[0];
+        $(form).attr('action', '/basket/add/' + selected_child + '/');
+        return true;
+    }
+    return false;
+}
