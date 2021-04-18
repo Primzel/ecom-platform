1a34a916a6bd23979c6cb778df5c5b6199f3b4ad
qasimgulzar <qasim.khokhar52@gmail.com>
2021-04-18 17:52:47 +0500

    [FIX] s3 bucket for tenant.

--- backends/storage_backends.py
+++ backends/storage_backends.py
@@ -1,5 +1,40 @@
+import logging
+
+from django.db import connection
 from storages.backends.s3boto3 import S3Boto3Storage
+from storages.utils import setting
+from multitenancy.models import Tenant
+
+log = logging.getLogger(__name__)
+
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
     file_overwrite = False
+
+    def __init__(self, **settings):
+        super().__init__(**settings)
+
+    def get_default_settings(self):
+        settings = super(MediaStorage, self).get_default_settings()
+        settings.update([{'bucket_name', self.tenant_bucket_name}])
+        return settings
+
+    @property
+    def tenant(self):
+        tenant = None
+        try:
+            tenant = Tenant.objects.get(schema_name=connection.schema_name)
+        except Exception as ex:
+            log.exception('Unable to get tenant', ex)
+        return tenant
+
+    @property
+    def tenant_bucket_name(self):
+        bucket_name = self.tenant.s3_bucket_name if self.tenant else setting('AWS_STORAGE_BUCKET_NAME')
+        log.debug(f'Using {bucket_name} for tenant {self.tenant.schema_name}')
+        return bucket_name
+
+    @property
+    def bucket(self):
+        return self.connection.Bucket(self.tenant_bucket_name)
