a03352471e4354ea6e5a0bc76e8fee3bb91d27b3
qasimgulzar <qasim.khokhar52@gmail.com>
2022-05-15 15:42:00 +0500

    🚑️ AWS_S3_SIGNATURE_VERSION

--- e_store_primzel/settings/aws.py
+++ e_store_primzel/settings/aws.py
@@ -20,3 +20,4 @@ AWS_SES_REGION_NAME = os.getenv('AWS_SES_REGION_NAME', 'us-east-2')
 AWS_SES_REGION_ENDPOINT = os.getenv('AWS_SES_REGION_ENDPOINT', 'email.us-east-2.amazonaws.com')
 STATICFILES_STORAGE = "backends.storage_backends.StaticFileStorage"
 AWS_QUERYSTRING_AUTH = os.getenv('AWS_QUERYSTRING_AUTH', False)
+AWS_S3_SIGNATURE_VERSION = os.getenv('AWS_S3_SIGNATURE_VERSION', 's3v4')
