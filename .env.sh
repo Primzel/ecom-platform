816184dd51c59712536918ebf2b75478115df4f1
Muhammad Qasim Gulzar <19186089+qasimgulzar@users.noreply.github.com>
2024-01-12 23:20:30 +0500

    Update .env.sh

--- .env.sh
+++ .env.sh
@@ -3,11 +3,11 @@ source .env/bin/activate
 
 export DJANGO_SETTINGS_MODULE=e_store_primzel.settings
 export PRIMZEL_DEBUG=False
-export POSTGRES_HOST=primzel-prod.catukxjh4orf.us-east-2.rds.amazonaws.com
+export POSTGRES_HOST=primzel-prod.us-east-2.rds.amazonaws.com
 export POSTGRES_USER=primzel
-export POSTGRES_PASSWORD=Primzel44332211
-export AWS_ACCESS_KEY_ID=AKIAJLCUH4TDT3G4ORYQ
-export AWS_SECRET_ACCESS_KEY=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
+export POSTGRES_PASSWORD=Primzel
+export AWS_ACCESS_KEY_ID=*******
+export AWS_SECRET_ACCESS_KEY=*******
 export AWS_STORAGE_BUCKET_NAME=primzel-prod-shared
-export AWS_S3_ENDPOINT_URL=https://primzel-prod-shared.s3.us-east-2.amazonaws.com/
+export AWS_S3_ENDPOINT_URL=https://bucket-shared.s3.us-east-2.amazonaws.com/
 export POSTGRES_DB_NAME=primzel-production
