1174996ab79c8640d1591cc04c2bb7775c41472b
qasim <qasim@primzel.com>
2025-02-17 16:47:11 +0500

    feat: add review stats to PDP

--- apps/catalogue/templatetags/primzel_tags.py
+++ apps/catalogue/templatetags/primzel_tags.py
@@ -1,6 +1,7 @@
+from celery.worker.state import total_count
 from django import template
 from oscar.apps.catalogue.models import ProductAttributeValue
-from oscar.apps.partner.models import StockRecord 
+from oscar.apps.partner.models import StockRecord
 
 register = template.Library()
 
@@ -28,7 +29,8 @@ def _get_variant_descriptors(product):
             num_in_stock = 0  # Handle the case where no stock record exists
 
         if num_in_stock > 0 or not product.product_class.track_stock:
-            for attribute_value in ProductAttributeValue.objects.filter(product=child).select_related('product', 'attribute'):
+            for attribute_value in ProductAttributeValue.objects.filter(product=child).select_related('product',
+                                                                                                      'attribute'):
                 attribute_name, value = attribute_value.attribute.name, attribute_value.value
 
                 # Add product IDs for attribute values
@@ -44,3 +46,73 @@ def render_variant_descriptors(product):
         'product': product,
         'descriptors': _get_variant_descriptors(product)
     }
+
+
+@register.filter
+def count_high_reviews(reviews, rating_threshold=3):
+    return reviews.filter(score__gte=rating_threshold).count()
+
+
+@register.filter
+def review_percentage_five_star(reviews):
+    total_count = reviews.count()
+    if not total_count:
+        return 0
+    return round((reviews.filter(score=5).count() / total_count) * 100)
+
+
+@register.filter
+def review_count_five_star(reviews):
+    return reviews.filter(score=5).count()
+
+
+@register.filter
+def review_percentage_four_star(reviews):
+    total_count = reviews.count()
+    if not total_count:
+        return 0
+    return round((reviews.filter(score=4).count() / total_count) * 100)
+
+
+@register.filter
+def review_count_four_star(reviews):
+    return reviews.filter(score=4).count()
+
+
+@register.filter
+def review_percentage_three_star(reviews):
+    total_count = reviews.count()
+    if not total_count:
+        return 0
+    return round((reviews.filter(score=3).count() / total_count) * 100)
+
+
+@register.filter
+def review_count_three_star(reviews):
+    return reviews.filter(score=3).count()
+
+
+@register.filter
+def review_percentage_two_star(reviews):
+    total_count = reviews.count()
+    if not total_count:
+        return 0
+    return round((reviews.filter(score=2).count() / total_count) * 100)
+
+
+@register.filter
+def review_count_two_star(reviews):
+    return reviews.filter(score=2).count()
+
+
+@register.filter
+def review_percentage_one_star(reviews):
+    total_count = reviews.count()
+    if not total_count:
+        return 0
+    return round((reviews.filter(score=1).count() / total_count) * 100)
+
+
+@register.filter
+def review_count_one_star(reviews):
+    return reviews.filter(score=1).count()
